2d65d48300b22d644c558aa3882204685c129b9c
KyleHouston84 <chariot124@gmail.com>
2018-08-25 11:36:16 -0700

    Portfolio structure complete.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header.component';
+import {
+	AboutComponent,
+	ContactComponent,
+	HeaderComponent,
+	HomeComponent,
+	WorkComponent
+} from './components';
 
 @NgModule({
   declarations: [
     AppComponent,
-	HeaderComponent
+	AboutComponent,
+	ContactComponent,
+	HeaderComponent,
+	HomeComponent,
+	WorkComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/index.ts
@@ -0,0 +1,5 @@
+export { HeaderComponent } from './header.component';
+export { HomeComponent } from './home.component';
+export { AboutComponent } from './about.component';
+export { WorkComponent } from './work.component';
+export { ContactComponent } from './contact.component';
